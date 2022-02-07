e6a64eb9402f5f0c5b84aa3792aa33b47f051e36
FHGH233 <99169383+FHGH233@users.noreply.github.com>
2022-02-07 18:21:37 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,9 +341,9 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return 'AvavaAVA';
-    if (score <= 99) return '给你一拳！';
-    if (score <= 149) return '好兄弟！';
+    if (score <= 49) return '就这？？';
+    if (score <= 99) return '差不多得了';
+    if (score <= 149) return '什么音游人';
     if (score <= 199) return '下雨了...';
     return '也不是不可以啦~';
 }
